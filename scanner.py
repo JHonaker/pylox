ac600061ef098d25da287730ae222cbf5bbafb25
JHonaker <honaker215@gmail.com>
2017-04-26 15:34:11 -0400

    Correctly implemented TokenType Enum

--- scanner.py
+++ scanner.py
@@ -1,25 +1,54 @@
 import lox
 from enum import Enum
 
-TokenType = Enum(
+class TokenType(Enum):
     # Single character tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN = 1
+    RIGHT_PAREN = 2
+    LEFT_BRACE = 3
+    RIGHT_BRACE = 4
+    COMMA = 5
+    DOT = 6
+    MINUS = 7
+    PLUS = 8
+    SEMICOLON = 9
+    SLASH = 10
+    STAR = 11
 
     # One or two character tokens
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
+    BANG = 12
+    BANG_EQUAL = 13
+    EQUAL = 14
+    EQUAL_EQUAL = 15
+    GREATER = 16
+    GREATER_EQUAL = 17
+    LESS = 18
+    LESS_EQUAL = 19
 
     # Literals
-    IDENTIFIER, STRING, NUMBER,
+    IDENTIFIER = 20
+    STRING = 21
+    NUMBER = 22
 
     # Keywords
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    AND = 23
+    CLASS = 24
+    ELSE = 25
+    FALSE = 26
+    FUN = 27
+    FOR = 28
+    IF = 29
+    NIL = 30
+    OR = 31
+    PRINT = 32
+    RETURN = 33
+    SUPER = 34
+    THIS = 35
+    TRUE = 36
+    VAR = 37
+    WHILE = 38
 
-    EOF
+    EOF = 39
 )
 
 class Token:
