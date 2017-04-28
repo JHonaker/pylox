7cd166a84cca3e53e1177247154a31604ae9de07
JHonaker <honaker215@gmail.com>
2017-04-28 13:37:42 -0400

    Created a meta-programming AST class generator

--- GenerateAST.py
+++ GenerateAST.py
@@ -1,9 +1,55 @@
 #! /usr/local/bin/python3
 
+import pdb
 import sys
 
-def main():
-    pass
+output_dir = None
+
+tab = "    " # Tab is four spaces
+
+base_desc = {
+    "Expr": {
+        "Unary": [["Token", "operator"], ["Expr", "right"]],
+        "Binary": [["Expr", "left"], ["Token", "operator"], ["Expr", "right"]],
+        "Grouping" : [["Expr", "expression"]],
+        "Literal" : [["Object", "value"]]
+    }
+}
+
+
+def defineAst(base_name, types):
+    path = output_dir + "/" + base_name + ".py"
+
+    with open(path, "w+") as con:
+        con.writelines(["class " + base_name + ":\n",
+                        tab + "pass\n\n"])
+        pdb.set_trace()
+        for expr_type, expr in types.items():
+            defineType(con, base_name, expr_type, expr)
+
+def defineType(con, base_name, class_name, fields):
+
+    types, names = zip(*fields)
+
+    field_str = ", ".join(names)
+
+    assert_stmts = [tab + tab +
+                    "assert isinstanceof(" + field[1] + ", " + field[0] + ")\n"
+                    for field in fields]
+
+    var_stmts = [tab + tab +
+                 "self." + name + " = " + name + "\n"
+                 for name in names]
+
+    con.write("\n")
+    con.writelines(["class " + class_name + "(" + base_name + "):\n"
+                    "",
+                    # The constructor
+                    tab + "def __init__(self, " +field_str + "):\n"])
+    con.writelines(assert_stmts)
+    con.write("\n")
+    con.writelines(var_stmts)
+    con.write("\n")
 
 
 if __name__ == "__main__":
@@ -11,4 +57,5 @@ if __name__ == "__main__":
         print("Usage: generate_ast <output directory>")
         sys.exit(1)
 
-    main()
+    output_dir = sys.argv[1]
+    defineAst("Expr", base_desc["Expr"])
