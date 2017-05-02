911b971f88d25ee84f48a67a197c79387b2c96dc
JHonaker <honaker215@gmail.com>
2017-05-02 10:34:37 -0400

    Implemented parser synchronization and basic error handling
    Also fixed circular importing and gave paser a reference to interpreter.

--- parser.py
+++ parser.py
@@ -1,15 +1,25 @@
-from scanner import TokenType, Token
 import grammar
+import scanner
 
+class ParseError(Exception):
+      """Raise for an unexpected token in the parser."""
 
 class Parser:
 
-      def __init__(self, token_list):
+      def __init__(self, interpreter, token_list):
+
+            self._interpreter = interpreter
 
             # The current head index in the token list
             self._current = 0
 
             self.token_list = token_list
+
+      def parse(self):
+            try:
+                  return self._expression()
+            except ParseError as error:
+                  return None
 
       def _match(self, *token_types):
             """Looks ahead one token. If the next token matches one of the
@@ -37,7 +47,7 @@ class Parser:
 
       def _is_at_end(self):
             """Returns True if the next token is an EOF."""
-            return self._peek().token_type == TokenType.EOF
+            return self._peek().token_type == scanner.TokenType.EOF
 
       def _peek(self):
             """Returns the token to be consumed next."""
@@ -45,7 +55,7 @@ class Parser:
 
       def _previous(self):
             """Returns the previous token in the list."""
-            return self._token_list[self._current - 1]
+            return self.token_list[self._current - 1]
 
       def _expression(self):
             """Matches based on the rule:
@@ -59,7 +69,7 @@ class Parser:
 
             # An Equality expression can match either a comparison
             # or comparison ((!= | ==) comparison)*
-            while self._match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL):
+            while self._match(scanner.TokenType.BANG_EQUAL, scanner.TokenType.EQUAL_EQUAL):
                   operator = self._previous()
                   right = self._comparison()
                   expr = grammer.Binary(expr, operator, right)
@@ -71,10 +81,10 @@ class Parser:
             comparison -> term ( ( > | >= | < | <= ) term)*"""
             expr = self._term()
 
-            while self._match(TokenType.GREATER,
-                              TokenType.GREATER_EQUAL,
-                              TokenType.LESS,
-                              TokenType.LESS_EQUAL):
+            while self._match(scanner.TokenType.GREATER,
+                              scanner.TokenType.GREATER_EQUAL,
+                              scanner.TokenType.LESS,
+                              scanner.TokenType.LESS_EQUAL):
                   operator = self._previous()
                   right = self._term()
                   expr = grammar.Binary(expr, operator, right)
@@ -86,7 +96,7 @@ class Parser:
             term -> factor ( ( - | + ) factor)*"""
             expr = self._factor()
 
-            while self._match(TokenType.MINUS, TokenType.PLUS):
+            while self._match(scanner.TokenType.MINUS, scanner.TokenType.PLUS):
                   operator = self._previous()
                   right = self._factor()
                   expr = grammar.Binary(expr, operator, right)
@@ -98,7 +108,7 @@ class Parser:
             factor -> unary ( ( / | * ) unary)*"""
             expr = self._unary()
 
-            while self._match(TokenType.SLASH, TokenType.STAR):
+            while self._match(scanner.TokenType.SLASH, scanner.TokenType.STAR):
                   operator = self._previous()
                   right = self._unary()
                   expr = grammar.Binary(expr, operator, right)
@@ -109,7 +119,7 @@ class Parser:
             """Matches based on the rule:
             unary -> ( ! | - ) unary
                    | primary"""
-            if self._match(TokenType.BANG, TokenType.MINUS):
+            if self._match(scanner.TokenType.BANG, scanner.TokenType.MINUS):
                   operator = self._previous()
                   right = self._unary()
                   return grammar.Unary(operator, right)
@@ -117,18 +127,54 @@ class Parser:
             return self._primary()
 
       def _primary(self):
-            if self._match(TokenType.FALSE):
+            if self._match(scanner.TokenType.FALSE):
                   return grammar.Literal(False)
-            elif self._match(TokenType.True):
+            elif self._match(scanner.TokenType.TRUE):
                   return grammar.Literal(True)
-            elif self._match(TokenType.NIL):
+            elif self._match(scanner.TokenType.NIL):
                   return grammar.Literal(None)
 
-            elif self._match(TokenType.NUMBER, TokenType.STRING):
+            elif self._match(scanner.TokenType.NUMBER, scanner.TokenType.STRING):
                   return grammar.Literal(self._previous().literal)
 
-            elif self._match(TokenType.LEFT_PAREN):
+            elif self._match(scanner.TokenType.LEFT_PAREN):
                   expr = self._expression()
-                  self._consume(TokenType.RIGHT_PAREN,
+                  self._consume(scanner.TokenType.RIGHT_PAREN,
                                 "Expect ')' after expression.")
                   return grammar.Grouping(expr)
+
+            raise self._error(self._peek(), "Expect expression.")
+
+      def _consume(self, token_type, msg):
+            """Attempts to consume the next token if it is the given type."""
+            if self._check(token_type):
+                  return self._advance()
+
+            raise self._error(self._peek(), msg)
+
+      def _error(self, token, msg):
+            """Returns a ParseError and logs an error with the interpreter."""
+            self._interpreter.parse_error(token, msg)
+            return ParseError()
+
+      def _synchronize(self):
+            self._advance()
+
+            while not self._is_at_end():
+                  if self._previous().token_type == scanner.TokenType.SEMICOLON:
+                        return
+
+                  end_statement_tokens  = [scanner.TokenType.CLASS,
+                                           scanner.TokenType.FUN,
+                                           scanner.TokenType.VAR,
+                                           scanner.TokenType.FOR,
+                                           scanner.TokenType.IF,
+                                           scanner.TokenType.WHILE,
+                                           scanner.TokenType.PRINT,
+                                           scanner.TokenType.RETURN]
+
+                  if self._peek().token_type in end_statement_tokens:
+                        return
+
+                  self._advance()
+
